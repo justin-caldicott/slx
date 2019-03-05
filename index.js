57a6a94485f025f84f1c6d4996c3164ce9528e0f
justin-caldicott <justin@caldicott.org>
2019-03-05 00:45:25 +0000

    now running commands

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const program = require('commander');
 const npmRun = require('npm-run');
+const uuid = require('uuid/v4');
 
+let cmdValue;
 program
-  .option('-no-sauce', 'Remove sauce')
-  .parse(process.argv);
+  .arguments('<cmd>')
+  .action((cmd) => {
+    cmdValue = cmd;
+  });
+program.parse(process.argv);
 
-const run = (command, args, options = {}) => {
-  // TODO: return promise and use async
-  const child = npmRun.spawn(
-    `${command}.cmd`, // TODO: cmd on windows, otherwise without.  Not sure why this is an issue now.
-    args.split(' '),
-    options,
-  );
+const run = (command, args, options = {}) => new Promise((resolve, reject) => {
+  const child = npmRun.spawn(command, args, options);
   child.stdout.pipe(process.stdout);
   child.stderr.pipe(process.stderr);
-  child.on('close', (code) => {
-    console.log(`finishedWithStatus ${code}`);
-  });
-  child.on('error', () => {
-    console.log('Failed to invoke serverless.  Is it installed?');
-  });
+  child.on('close', code => resolve(code));
+  child.on('error', error => reject(error));
+});
+
+const main = async () => {
+  const stage = uuid().split('-')[0];
+  process.env.STAGE = stage;
+  // TODO: cmd on windows, otherwise without.  Not sure why this is an issue now.
+  try {
+    await run('sls.cmd', `deploy --stage ${stage}`.split(' '));
+    const argsIndex = process.argv.indexOf(cmdValue) + 1;
+    const args = process.argv.slice(argsIndex);
+    await run(cmdValue, args);
+  } finally {
+    await run('sls.cmd', `remove --stage ${stage}`.split(' '));
+  }
 };
 
-run('sls', 'deploy');
-
-run('sls', 'remove');
+main();
--- index.test.js
+++ index.test.js
@@ -3,9 +3,13 @@ const npmRun = require('npm-run');
 
 describe('slx', () => {
   it('deploys a stack', () => {
-    const child = npmRun.spawnSync('node', '../index.js'.split(' '), { cwd: `${__dirname}/test` });
-    console.log(child.stdout.toString());
-    console.error(child.stderr.toString());
+    const child = npmRun.spawnSync('node', '../index.js node -v'.split(' '), { cwd: `${__dirname}/test` });
+    if (child.stdout && child.stdout.toString()) {
+      console.log(child.stdout.toString());
+    }
+    if (child.stderr && child.stderr.toString()) {
+      console.error(child.stderr.toString());
+    }
     expect(child.stdout.toString()).toContain('Packaging service...');
   });
 });
